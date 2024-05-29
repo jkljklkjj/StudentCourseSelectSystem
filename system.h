b77414df95d718ae7620d6e47e294b0b64fa39e6
jkljklkjj <2998568539@qq.com>
2024-05-29 08:14:34 +0800

    第2次迭代

--- system.h
+++ system.h
@@ -2,19 +2,20 @@
 #include <iostream>
 using namespace std;
 
-class Login {
-  private:
-    string password;
-    int times;
+class Login
+{
+private:
+  string password;
+  int times;
 
-  public:
-    Login();
-    void begin();         // 进入登录系统
-    bool check(string);   // 检查密码是否正确
-    long long getid();    // 获取学号
-    string getpassword(); // 获取密码
-    void signup();        // 注册
-    void start();         // 进入系统
-    static string caesarEncrypt(const string &); // 凯撒加密
-    static string caesarDecrypt(const string &); // 凯撒解密
+public:
+  Login();
+  void begin();                                // 进入登录系统
+  bool check(string);                          // 检查密码是否正确
+  long long getid();                           // 获取学号
+  string getpassword();                        // 获取密码
+  void signup();                               // 注册
+  void start();                                // 进入系统
+  static string caesarEncrypt(const string &); // 凯撒加密
+  static string caesarDecrypt(const string &); // 凯撒解密
 };
